5c2ffa47f38441740d02e03057944f9fd5f2ba84
Thiago Oliveira <thiagosilvaoliveira66@gmail.com>
2018-09-28 15:26:11 -0300

    Trocando matriz por vetor

--- js/script.js
+++ js/script.js
@@ -8,11 +8,16 @@ var last = null;
 //Single player ou não
 var single = true;
 //Tabuleiro das jogadas do jogo
-var velha = [
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined]
-];
+var velha = new Array(9).fill(-5);
+/*
+====Tabuleiro====
+[
+    -5 ,-5 ,-5,
+    -5, -5, -5,
+    -5, -5, -5
+]
+=================
+*/
 //Mapa cordenadas do jogo
 var mapaTabuleiro = [
     [{ x: 150, y: 150 }, { x: 150, y: 350 }, { x: 150, y: 550 }],
@@ -61,128 +66,136 @@ function mousePos(event) {
 function jogada(event, pc) {
     var cords = event == null ? pc : mousePos(event);
     if (cords.x < 200) {
+        //Quadrado 1 da primeira linha
         if (cords.y < 200) {
-            if (velha[0][0] != undefined) {
+            if (velha[0] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[0][0] = 1;
+                velha[0] = 1;
                 desenhaX(50, 50);
 
             } else {
-                velha[0][0] = 0;
+                velha[0] = 0;
                 desenhaBola(50, 50);
 
             }
+            //Quadrado 1 da segunda linha
         } else if (cords.y < 400) {
-            if (velha[1][0] != undefined) {
+            if (velha[3] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[1][0] = 1;
+                velha[3] = 1;
                 desenhaX(50, 250);
 
             } else {
-                velha[1][0] = 0;
+                velha[3] = 0;
                 desenhaBola(50, 250);
 
             }
+            //Quadrado 1 da terceira linha
         } else if (cords.y < 600) {
-            if (velha[2][0] != undefined) {
+            if (velha[6] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[2][0] = 1
+                velha[6] = 1
                 desenhaX(50, 450);
 
             } else {
-                velha[2][0] = 0;
+                velha[6] = 0;
                 desenhaBola(50, 450);
 
             }
         }
     }
     else if (cords.x < 400) {
+        //Quadrado 2 da primeira linha
         if (cords.y < 200) {
-            if (velha[0][1] != undefined) {
+            if (velha[1] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[0][1] = 1;
+                velha[1] = 1;
                 desenhaX(250, 50);
 
             } else {
-                velha[0][1] = 0;
+                velha[1] = 0;
                 desenhaBola(250, 50);
 
             }
+            //Quadrado 2 da segunda linha
         } else if (cords.y < 400) {
-            if (velha[1][1] != undefined) {
+            if (velha[4] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[1][1] = 1;
+                velha[4] = 1;
                 desenhaX(250, 250);
 
             } else {
-                velha[1][1] = 0;
+                velha[4] = 0;
                 desenhaBola(250, 250);
 
             }
+            //Quadrado 2 da terceira linha
         } else if (cords.y < 600) {
-            if (velha[2][1] != undefined) {
+            if (velha[7] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[2][1] = 1;
+                velha[7] = 1;
                 desenhaX(250, 450);
 
             } else {
-                velha[2][1] = 0;
+                velha[7] = 0;
                 desenhaBola(250, 450);
 
             }
         }
     }
     else if (cords.x < 600) {
+        // Quadrado 3 da primeira linha
         if (cords.y < 200) {
-            if (velha[0][2] != undefined) {
+            if (velha[2] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[0][2] = 1;
+                velha[2] = 1;
                 desenhaX(450, 50);
 
             } else {
-                velha[0][2] = 0;
+                velha[2] = 0;
                 desenhaBola(450, 50);
 
             }
+            // Quadrado 3 da segunda linha
         } else if (cords.y < 400) {
-            if (velha[1][2] != undefined) {
+            if (velha[5] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[1][2] = 1;
+                velha[5] = 1;
                 desenhaX(450, 250);
 
             } else {
-                velha[1][2] = 0;
+                velha[5] = 0;
                 desenhaBola(450, 250);
 
             }
+            // Quadrado 3 da terceira linha
         } else if (cords.y < 600) {
-            if (velha[2][2] != undefined) {
+            if (velha[8] != -5) {
                 return false;
             }
             if ((cont % 2) == 1) {
-                velha[2][2] = 1;
+                velha[8] = 1;
                 desenhaX(450, 450);
 
             } else {
-                velha[2][2] = 0;
+                velha[8] = 0;
                 desenhaBola(450, 450);
-
             }
         }
     }
@@ -207,10 +220,8 @@ function computador() {
 //Verifica se todos os campos foram preenchidos
 function jogoEncerrado() {
     for (var i = 0; i < velha.length; i++) {
-        for (var j = 0; j < velha[i].length; j++) {
-            if (velha[i][j] == undefined) {
-                return false;
-            }
+        if (velha[i] == -5) {
+            return false;
         }
     }
     return true;
@@ -254,7 +265,7 @@ function desenhaX(x, y) {
 //Verifica se tem um vencedor e desenha linha
 function verificaVitoria() {
     if (!temVencedor) {
-        if ((velha[0][0] + velha[0][1] + velha[0][2]) == 3 || (velha[0][0] + velha[0][1] + velha[0][2]) == 0) {
+        if ((velha[0] + velha[1] + velha[2]) == 3 * last) {
             context.beginPath();
             context.moveTo(0, 100);
             context.lineTo(600, 100);
@@ -264,7 +275,7 @@ function verificaVitoria() {
             context.stroke();
             temVencedor = true;
         }
-        else if ((velha[1][0] + velha[1][1] + velha[1][2]) == 3 || (velha[1][0] + velha[1][1] + velha[1][2]) == 0) {
+        else if ((velha[3] + velha[4] + velha[5]) == 3 * last) {
             context.beginPath();
             context.moveTo(0, 300);
             context.lineTo(600, 300);
@@ -274,7 +285,7 @@ function verificaVitoria() {
             context.stroke();
             temVencedor = true;
         }
-        else if ((velha[2][0] + velha[2][1] + velha[2][2]) == 3 || (velha[2][0] + velha[2][1] + velha[2][2]) == 0) {
+        else if ((velha[6] + velha[7] + velha[8]) == 3 * last) {
             context.beginPath();
             context.moveTo(0, 500);
             context.lineTo(600, 500);
@@ -284,7 +295,7 @@ function verificaVitoria() {
             context.stroke();
             temVencedor = true;
         }
-        else if ((velha[0][0] + velha[1][0] + velha[2][0]) == 3 || (velha[0][0] + velha[1][0] + velha[2][0]) == 0) {
+        else if ((velha[0] + velha[3] + velha[6]) == 3 * last) {
             context.beginPath();
             context.moveTo(100, 0);
             context.lineTo(100, 600);
@@ -294,7 +305,7 @@ function verificaVitoria() {
             context.stroke();
             temVencedor = true;
         }
-        else if ((velha[0][1] + velha[1][1] + velha[2][1]) == 3 || (velha[0][1] + velha[1][1] + velha[2][1]) == 0) {
+        else if ((velha[1] + velha[4] + velha[7]) == 3 * last) {
             context.beginPath();
             context.moveTo(300, 0);
             context.lineTo(300, 600);
@@ -304,7 +315,7 @@ function verificaVitoria() {
             context.stroke();
             temVencedor = true;
         }
-        else if ((velha[0][2] + velha[1][2] + velha[2][2]) == 3 || (velha[0][2] + velha[1][2] + velha[2][2]) == 0) {
+        else if ((velha[2] + velha[5] + velha[8]) == 3 * last) {
             context.beginPath();
             context.moveTo(500, 0);
             context.lineTo(500, 600);
@@ -314,7 +325,7 @@ function verificaVitoria() {
             context.stroke();
             temVencedor = true;
         }
-        else if ((velha[0][2] + velha[1][1] + velha[2][0]) == 3 || (velha[0][2] + velha[1][1] + velha[2][0]) == 0) {
+        else if ((velha[2] + velha[4] + velha[6]) == 3 * last) {
             context.beginPath();
             context.moveTo(600, 0);
             context.lineTo(0, 600);
@@ -324,7 +335,7 @@ function verificaVitoria() {
             context.stroke();
             temVencedor = true;
         }
-        else if ((velha[2][2] + velha[1][1] + velha[0][0]) == 3 || (velha[2][2] + velha[1][1] + velha[0][0]) == 0) {
+        else if ((velha[0] + velha[4] + velha[8]) == 3 * last) {
             context.beginPath();
             context.moveTo(0, 0);
             context.lineTo(600, 600);
@@ -351,11 +362,7 @@ function alterPlayerMode(param) {
 //Limpa o jogo
 function limpar() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    velha = [
-        [undefined, undefined, undefined],
-        [undefined, undefined, undefined],
-        [undefined, undefined, undefined]
-    ];
+    velha.fill(-5);
     temVencedor = false;
     last = null;
     cont = 0;
@@ -371,15 +378,15 @@ function zeraPontuacao() {
 //Codigo destinado a aplica algoritimo de IA ao jogo
 // Em contrução
 function winning(player) {
-    if(
-        velha[0][0] + velha[0][1] + velha[0][2] == player || 
-        velha[1][0] + velha[1][1] + velha[1][2] == player || 
-        velha[2][0] + velha[2][1] + velha[2][2] == player || 
-        velha[0][0] + velha[1][0] + velha[2][0] == player || 
-        velha[0][1] + velha[1][1] + velha[2][1] == player || 
-        velha[0][2] + velha[1][2] + velha[2][2] == player || 
-        velha[0][2] + velha[1][1] + velha[2][0] == player || 
-        velha[2][2] + velha[1][1] + velha[0][0] == player 
+    if (
+        velha[0] + velha[3] + velha[6] == (3 * player) ||
+        velha[1] + velha[4] + velha[7] == (3 * player) ||
+        velha[2] + velha[5] + velha[8] == (3 * player) ||
+        velha[0] + velha[1] + velha[2] == (3 * player) ||
+        velha[3] + velha[4] + velha[5] == (3 * player) ||
+        velha[6] + velha[7] + velha[8] == (3 * player) ||
+        velha[0] + velha[4] + velha[8] == (3 * player) ||
+        velha[2] + velha[4] + velha[6] == (3 * player)
     ) {
         return true;
     } else {
@@ -391,7 +398,7 @@ function disponivel(_velha) {
     return _velha.filter(el => (el != jogador && el != ia));
 }
 
-function minmax(_velha) { 
+function minmax(_velha) {
     var _av;
 
     if (winning(jogador)) {
